62e9e79a3cfa149622f0b5bf17fcd7338047dc5a
Edwin Mogollón <edwin.jme@hotmail.com>
2021-02-05 06:41:05 -0300

    fixes incidents

--- resources/views/panel/incidents/details.blade.php
+++ resources/views/panel/incidents/details.blade.php
@@ -60,12 +60,12 @@
                                      alt="avatar">
                             </div>
                             <div class="comments-description">
-                                <p class="date"><b>Fecha:</b> 15/01/2021 </p>
-                                <p class="user-comment"><b>Sofia Singer dijo:</b></p>
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                                    when an unknown printer took a galley of type and scrambled it to make a type
-                                    specimen book. It has survived not only five centuries</p>
+                                <p class="date"><b>Fecha:</b>
+                                    {{ date('d/m/Y',$comment->created_at ) }}
+
+                                </p>
+                                <p class="user-comment"><b>{{$comment->nombre}} dijo:</b></p>
+                                <p>{{$comment->content}}</p>
                             </div>
 
                         </div>
