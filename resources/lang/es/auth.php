cdd4891cf41517626b72831ec5bc32d19f51d5e4
sofia <sofia@broobe.com>
2021-02-19 00:06:38 -0300

    Traducciones

---
+++ resources/lang/es/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Estas credenciales no estan en nuestro sistema',
+    'password' => 'La contraseña provista es incorrecta',
+    'throttle' => 'Muchos intentos fallidos de iniciar sesión. Por favor intente nuevamente en :seconds segundos.',
+
+];
